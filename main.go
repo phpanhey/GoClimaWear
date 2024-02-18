2908b6e2bacad8f3a9a60b999d53a7cf389af981
ppp <philipp.panhey@gmail.com>
2024-02-18 20:10:30 +0100

    chore: parsing arguments

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-// print hello world
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	temperature, humidity := getTemperatureAndHumidity()
+
+	fmt.Println("Temperature:", temperature)
+	fmt.Println("Humidity:", humidity)
+
+}
+
+func getTemperatureAndHumidity() (float64, float64) {
+	tempStr := flag.String("temperature", "20", "The current temperature")
+	humidityStr := flag.String("humidity", "50", "The current humidity")
+	flag.Parse()
+	temp, err := strconv.ParseFloat(*tempStr, 64)
+	if err != nil {
+		fmt.Println("Error parsing temperature:", err)
+
+	}
+	humidity, err := strconv.ParseFloat(*humidityStr, 64)
+	if err != nil {
+		fmt.Println("Error parsing humidity:", err)
+
+	}
+	return temp, humidity
 }
